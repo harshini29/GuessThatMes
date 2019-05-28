8276ffff337007100a49342204ad9913caf1de18
Harshini <harshini1@Harshinis-MacBook-Air.local>
2019-05-28 07:26:41 -0700

    Initial commit

---
+++ app/src/main/java/com/example/guessthatmess/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.guessthatmess;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonLogin,buttonSignup;
+    private EditText txtEmail,txtPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        buttonLogin = findViewById(R.id.login_id);
+        buttonSignup = findViewById(R.id.signup_id);
+
+        txtEmail = findViewById(R.id.username_id);
+        txtPassword = findViewById(R.id.password_id);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent= new Intent(getApplication(),GameZoneActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent= new Intent(getApplication(), SignupActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+}
